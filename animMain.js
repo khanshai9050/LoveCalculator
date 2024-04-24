488794c707e15e8be02b1f5fe64f2ec4a22397f6
khanshai9050 <167965233+khanshai9050@users.noreply.github.com>
2024-04-24 16:28:21 +0700

    added animmain.html

---
+++ animMain.js
@@ -0,0 +1,7 @@
+
+gsap.from('.logo', {opacity: 0, duration: 1, delay: 2, y:10})
+gsap.from('.title', {opacity: 0, duration: 1, delay: 1.6, y:30})
+gsap.from('.description', {opacity: 0, duration: 1, delay: 1.8, y:30})
+gsap.from('.btn', {opacity: 0, duration: 1, delay: 2.1, y:30})
+
+
